355bdf501e4c8384e33bed7135a006651a91f648
David Morton <davidmortonmail@gmail.com>
2015-02-05 23:20:32 +0000

    Updated WorldManager

--- Classes/WorldManager.h
+++ Classes/WorldManager.h
@@ -11,42 +11,17 @@ USING_NS_CC;
 
 class WorldManager
 {
-public:
-	// typedefs
-	typedef std::shared_ptr<Player> spPlayer;
-	typedef std::shared_ptr<Enemy> spEnemy;
-	
+public:	
 	virtual bool init();	
-	virtual void cleanUp(); 
-	
-	// getters
+	virtual void cleanUp(); 	
 	static WorldManager* getInstance(); 
-	//spPlayer getPlayer(){ return m_pPlayer; };
 	Player* getPlayer(){ return m_pPlayer; };
-	spEnemy getEnemy(){ return m_pEnemy; };
-
-	// setters
 	void setPlayer(Player* player){ m_pPlayer = player; };
-	void setEnemy(spEnemy enemy){ m_pEnemy = enemy; };
-
-	void scaleSpriteToFullScreenSize(Sprite* sprite);
-
-private:
-	// private *tors and instance	
+private:	
 	WorldManager(){ this->init(); };  
-	~WorldManager(){ this->cleanUp(); }; 	
-	
-	static WorldManager* m_Instance;
-
-	Size m_sVisibleSize;
-	Vec2 m_vOrigin;
-
-	//spPlayer m_pPlayer;
+	~WorldManager(){ this->cleanUp(); }; 		
+	static WorldManager* m_pInstance;	
 	Player* m_pPlayer;
-	spEnemy m_pEnemy;
-	
-	
-	
 };
 
 #endif
