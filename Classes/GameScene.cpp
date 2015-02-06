a26649e8c4fd8b9908ad0f3134f8916c24259d5b
David Morton <davidmortonmail@gmail.com>
2015-02-06 21:13:19 +0000

    fixed floor function

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -77,7 +77,7 @@ bool GameScene::init()
 	// Player			
 	Player* playerSprite = Player::create("sprites/Player.png");		
 	playerSprite->setPosition(Vec2(((visibleSize.width / 3) * 1) + origin.x,		
-		WorldManager::getInstance()->getFLoorSprite()->getPositionY() + 		
+		WorldManager::getInstance()->getFloorSprite()->getPositionY() + 		
 		playerSprite->getContentSize().height / 2));
 	auto playerPhysicsBody = PhysicsBody::createBox(playerSprite->getContentSize(), PHYSICSBODY_MATERIAL_DEFAULT);
 	playerSprite->setPhysicsBody(playerPhysicsBody);
