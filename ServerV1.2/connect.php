08c8e75a9a2f747ddcb9477a1a5f6dd3d1dcd577
David Morton <davidmortonmail@gmail.com>
2015-03-27 17:15:54 +0000

    Updated website, database and bakc end statistics

--- ServerV1.2/connect.php
+++ ServerV1.2/connect.php
@@ -1,7 +1,23 @@
 <?php
+/*
+	Copyright (c) 2015 David Morton, Donnchadh Murphy, Georgina Sheehan, Tiago Oliveira
+
+	http://www.grandtheftmuffins.esy.es
+
+	Third year games design and development project. Grand Theft Muffins endless runner game
+	written in C++ using the Cocos2dx game engine. http://www.cocos2d-x.org. And back-end game 
+	analytics and statistics system built using a LAMP stack, Linux, Apache, MySQL and PHP. Hosted
+	locally and remotely.
+
+	connect.php
+
+	Description: Connect will handle creating a connection to the MySQL database. The hostname is localhost because
+	the PHP scripts run on a machine with the same IP address as the MySQL database. Only the login details and database
+	name will be modified for locally hosted servers and databases.
+*/
 function connect()
 {
-	// Database login details
+	// Remote database login details
 	$username = "u583858232_david";
 	$password = "davidGTMDB88";
 	$hostname = "localhost";
