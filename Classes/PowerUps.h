a63ce59b020b5b938783360919dfed70793a4a60
David Morton <davidmortonmail@gmail.com>
2015-03-27 22:14:56 +0000

    final updates made

--- Classes/PowerUps.h
+++ Classes/PowerUps.h
@@ -1,14 +1,15 @@
 /*
-Copyright (c) 2015 David Morton, Donnchadh Murphy, Georgina Sheehan, Tiago Oliveira
+	Copyright (c) 2015 David Morton, Donnchadh Murphy, Georgina Sheehan, Tiago Oliveira
 
-http://www.grandtheftmuffins.esy.es/
+	http://www.grandtheftmuffins.esy.es/
 
-Third year games design and development project. Grand Theft Muffins endless runner game
-written in C++ using the Cocos2dx game engine. http://www.cocos2d-x.org
+	Third year games design and development project. Grand Theft Muffins endless runner game
+	written in C++ using the Cocos2dx game engine. http://www.cocos2d-x.org. Back-end game analytics
+	and statistics system built using a LAMP stack, Linux, Apache, MySQL and PHP. Hosted locally and remotely.
 
-PowerUps.h
+	PowerUps.h
 
-Description: Creates a PowerUp abstract class 
+	Description: Creates a PowerUp abstract class 
 
 */
 #ifndef POWERUPS_H_
